c5bb0aa84d5e33b104a7cc83bb8300611ca4d100
Việt Anh Bùi <vietanhbui2000@gmail.com>
2020-04-07 23:18:51 +0700

    Delete files

--- luci-app-ttyd/luasrc/controller/ttyd.lua
+++
@@ -1,9 +0,0 @@
-module("luci.controller.ttyd", package.seeall)
-
-function index()
-	if not (luci.sys.call("pidof ttyd > /dev/null") == 0) then
-		return
-	end
-	
-	entry({"admin", "services", "ttyd"}, template("ttyd"), _("ttyd"), 10).leaf = true
-end
