41298e15a9de4b0585a831f67187fa5f8ee35249
Việt Anh Bùi <vietanhbui2000@gmail.com>
2020-04-20 03:26:36 +0700

    Update filebrowser.lua

--- vietanhbui2000/luci-app-filebrowser/luasrc/controller/filebrowser.lua
+++ vietanhbui2000/luci-app-filebrowser/luasrc/controller/filebrowser.lua
@@ -141,52 +141,106 @@ function scandir(directory)
 end
 
 MIME_TYPES = {
-    ["txt"]   = "text/plain";
-    ["conf"]   = "text/plain";
-    ["ovpn"]   = "text/plain";
-    ["log"]   = "text/plain";
-    ["js"]    = "text/javascript";
-    ["json"]    = "application/json";
-    ["css"]   = "text/css";
-    ["htm"]   = "text/html";
-    ["html"]  = "text/html";
-    ["patch"] = "text/x-patch";
-    ["c"]     = "text/x-csrc";
-    ["h"]     = "text/x-chdr";
-    ["o"]     = "text/x-object";
-    ["ko"]    = "text/x-object";
+    ["aac"]   = "audio/aac";
+    ["abw"]   = "application/x-abiword";
+    ["arc"]   = "application/x-freearc";
+    ["avi"]   = "video/x-msvideo";
+    ["azw"]   = "application/vnd.amazon.ebook";
 
+    ["bin"]   = "application/octet-stream";
     ["bmp"]   = "image/bmp";
+    ["bz"]   = "application/x-bzip";
+    ["bz2"]   = "application/x-bzip2";
+
+    ["csh"]   = "application/x-csh";
+    ["css"]   = "text/css";
+    ["csv"]   = "text/csv";
+
+    ["doc"]   = "application/msword";
+    ["docx"]   = "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
+
+    ["eot"]   = "application/vnd.ms-fontobject";
+    ["epub"]   = "application/epub+zip";
+
+    ["gz"]   = "application/gzip";
     ["gif"]   = "image/gif";
-    ["png"]   = "image/png";
+
+    ["htm"]   = "text/html";
+    ["html"]   = "text/html";
+
+    ["ico"]   = "image/vnd.microsoft.icon";
+    ["ics"]   = "text/calendar";
+
+    ["jar"]   = "application/java-archive";
+    ["jpeg"]   = "image/jpeg";
     ["jpg"]   = "image/jpeg";
-    ["jpeg"]  = "image/jpeg";
+    ["js"]   = "text/javascript";
+    ["json"]   = "application/json";
+    ["jsonld"]   = "application/ld+json";
+
+    ["mid"]   = "audio/midi";
+    ["mid"]   = "audio/x-midi";
+    ["midi"]   = "audio/midi";
+    ["midi"]   = "audio/x-midi";
+    ["mjs"]   = "text/javascript";
+    ["mp3"]   = "audio/mpeg";
+    ["mp4"]   = "video/mp4";
+    ["mpeg"]   = "video/mpeg";
+    ["mpkg"]   = "application/vnd.apple.installer+xml";
+
+    ["odp"]   = "application/vnd.oasis.opendocument.presentation";
+    ["ods"]   = "application/vnd.oasis.opendocument.spreadsheet";
+    ["odt"]   = "application/vnd.oasis.opendocument.text";
+    ["oga"]   = "audio/ogg";
+    ["ogv"]   = "video/ogg";
+    ["ogx"]   = "application/ogg";
+    ["opus"]   = "audio/opus";
+    ["otf"]   = "font/otf";
+
+    ["png"]   = "image/png";
+    ["pdf"]   = "application/pdf";
+    ["php"]   = "application/php";
+    ["ppt"]   = "application/vnd.ms-powerpoint";
+    ["pptx"]   = "application/vnd.openxmlformats-officedocument.presentationml.presentation";
+
+    ["rar"]   = "application/vnd.rar";
+    ["rtf"]   = "application/rtf";
+
+    ["sh"]   = "application/x-sh";
     ["svg"]   = "image/svg+xml";
+    ["swf"]   = "application/x-shockwave-flash";
+
+    ["tar"]   = "application/x-tar";
+    ["tif"]   = "image/tiff";
+    ["tiff"]   = "image/tiff";
+    ["ts"]   = "video/mp2t";
+    ["ttf"]   = "font/ttf";
+    ["txt"]   = "text/plain";
+
+    ["vsd"]   = "application/vnd.visio";
+
+    ["wav"]   = "audio/wav";
+    ["weba"]   = "audio/webm";
+    ["webm"]   = "video/webm";
+    ["webp"]   = "image/webp";
+    ["woff"]   = "font/woff";
+    ["woff2"]   = "font/woff2";
+
+    ["xhtml"]   = "application/xhtml+xml";
+    ["xls"]   = "application/vnd.ms-excel";
+    ["xlsx"]   = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+    ["xml"]   = "application/xml";
+    ["xml"]   = "text/xml";
+    ["xul"]   = "application/vnd.mozilla.xul+xml";
 
     ["zip"]   = "application/zip";
-    ["pdf"]   = "application/pdf";
-    ["xml"]   = "application/xml";
-    ["xsl"]   = "application/xml";
-    ["doc"]   = "application/msword";
-    ["ppt"]   = "application/vnd.ms-powerpoint";
-    ["xls"]   = "application/vnd.ms-excel";
-    ["odt"]   = "application/vnd.oasis.opendocument.text";
-    ["odp"]   = "application/vnd.oasis.opendocument.presentation";
-    ["pl"]    = "application/x-perl";
-    ["sh"]    = "application/x-shellscript";
-    ["php"]   = "application/x-php";
-    ["deb"]   = "application/x-deb";
-    ["iso"]   = "application/x-cd-image";
-    ["tgz"]   = "application/x-compressed-tar";
 
-    ["mp3"]   = "audio/mpeg";
-    ["ogg"]   = "audio/x-vorbis+ogg";
-    ["wav"]   = "audio/x-wav";
+    ["3gp"]   = "video/3gpp";
+    ["3gp"]   = "audio/3gpp";
+    ["3g2"]   = "video/3gpp2";
+    ["3g2"]   = "audio/3gpp2";
 
-    ["mpg"]   = "video/mpeg";
-    ["mpeg"]  = "video/mpeg";
-    ["avi"]   = "video/x-msvideo";
-    ["mp4"]   = "video/mp4";
+    ["7z"]   = "application/x-7z-compressed";
 }
 
 function to_mime(filename)
