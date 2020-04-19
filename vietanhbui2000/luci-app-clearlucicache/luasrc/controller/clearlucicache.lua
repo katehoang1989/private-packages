0aca5ecbd68470bf0af943c32cbfe1c43bd6e843
Việt Anh Bùi <vietanhbui2000@gmail.com>
2020-04-19 22:11:17 +0700

    Upload files

---
+++ vietanhbui2000/luci-app-clearlucicache/luasrc/controller/clearlucicache.lua
@@ -0,0 +1,9 @@
+module("luci.controller.clearlucicache",package.seeall)
+
+function index()
+	entry({"admin","services","clearlucicache"}, call("clearlucicache"), _("Clear LuCI Cache"), 9998)
+end
+function clearlucicache()
+	luci.sys.call("rm /tmp/luci-modulecache/* /tmp/luci-indexcache")
+	luci.http.redirect(luci.dispatcher.build_url("admin/status"))
+end
